a7c885ef75b0d86216e0e5a2ddcbd4a11131e0e2
web3saiful <web3saiful@gmail.com>
2025-11-09 23:19:54 +0600

    'Test'

---
+++ lecture1.sol
@@ -0,0 +1,155 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.10;
+
+
+
+// Introduction to Solidity
+// contract MyFirstContract{
+//     uint256 public constant myInt=123;
+//     address public immutable myAddress;
+// }
+
+
+
+
+
+// // stroring values in Contract
+// contract First{
+
+// int public num=18;
+
+// string public greetng="welcome";
+
+// bool public isActive=false;
+
+// address public myAddress=0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+// function getNumber()public view returns(int){
+//     return num;
+//    }
+// }
+
+
+
+
+
+
+
+
+
+// Storing Data
+
+// contract First{
+//     string public studentName;
+//     int public studentAge;
+//     string public courseName;
+//     bool public isEnrolled;
+ 
+//     function setProfile(string memory name,int age ,string memory course)public{
+//       studentName=name;
+//       studentAge=age;
+//       courseName=course;
+//       isEnrolled=true;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// // variables & Data Types
+// contract second{
+
+// // Numbers
+
+// int public num1 = -10;
+
+// uint public number=20;
+
+// uint8 public smallNumber=255;
+// uint16 public mediumNumber=50000;
+// uint256 public largeNumber=50000000;
+
+// string public message="Hello NonAcademy";
+// string public myName="Ai Arif";
+
+// //boolean
+
+// bool public isActive=true;
+// bool public isComplete=false;
+
+// // address
+
+// address public myAddress=0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+
+
+
+
+// uint public age=20;
+// uint public price=100;
+// uint public totalSupply=100000;
+
+// int public temperature= -5;
+// int public myId=10000;
+
+// function updateNumbers(uint newAge, int temp)public {
+//     age=newAge;
+//     temperature=temp;
+
+//      }
+
+//   }
+
+
+
+
+
+
+
+
+
+
+
+// // Mathemetical Operation
+
+// contract MathemeticalOpertion{
+//     uint public score=10;
+
+//     //Addition
+//     function addToScore(uint points)public {
+//         score+=points;
+//     }
+
+//     // Multiplication
+//     function multiplyScore(uint points)public{
+//         score*=points;
+//     }
+
+//     //Subtraction
+//     function subtractionFunction(uint points)public {
+//         require(score>=points, "Score cannot be negative");
+//         score=score-points;
+//     }
+//     //Division
+//     function dividedScore(uint number)public{
+//         require(number>0,"Cannot divide by zero");
+//         score/=number;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
