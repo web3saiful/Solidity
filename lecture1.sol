1d18508a516c3930a07634006215bba7e449bbf5
Saiful Islam <web3saiful@gmail.com>
2025-12-13 23:14:21 +0600

    Update lecture1.sol

--- lecture1.sol
+++ lecture1.sol
@@ -37,6 +37,36 @@ pragma solidity ^0.8.10;
 
 
 
+
+
+// // stroring values in Contract
+// contract First{
+
+// int public num=18;
+
+// string public greetng="welcome";
+
+// bool public isActive=false;
+
+// address public myAddress=0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+// function getNumber()public view returns(int){
+//     return num;
+//    }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Storing Data
 
 // contract First{
